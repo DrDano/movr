ed7f6f36222072b1060697b6078d050022359891
DrDano <danielharned@gmail.com>
2023-01-17 20:45:13 -0800

    working #time 1h #comment adding auth0 and passport dependencies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
+const expressSession = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false
+}
+
+app.get("env") === "production" && (session.cookie.secure = true);
 
 (async function connectMongoose() {
   try {
@@ -17,11 +31,13 @@ app.use(express.urlencoded({ extended: true }));
       console.log(err);
     })
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 })()
 
 app.use(require('./routes'));
+
+app.use(expressSession(session));
 
 // Use this to log mongo queries
 mongoose.set('debug', true);
